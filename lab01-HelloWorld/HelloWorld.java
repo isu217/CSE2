9580561321c980f93f96bc03c88bc90d6ec5dfa2
isu217 <isu217@lehigh.edu>
2014-08-29 14:48:47 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,30 @@
+//////////////////////////////////////////////////////////
+//Ian Udvarhelyi
+//CSE2
+//isu217
+//Lab01
+//Hello World Program
+//
+//Compile the program
+//      javac HelloWorld.java
+//  run program
+//      java HelloWorld
+
+//  define class
+public class HelloWorld{
+    
+//  add main method
+public static void main(String[] args) {
+    
+//  print statement with box aroung it
+    System.out.println("######################");
+    System.out.println("######################");
+    System.out.println("#                    #");
+    System.out.println("# Hello World 2014!! #");
+    System.out.println("#                    #");
+    System.out.println("######################");
+    System.out.println("######################");
+    System.out.println("test");
+    
+    }
+}
