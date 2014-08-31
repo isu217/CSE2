8b572b2724005c19006aa515c48892c9b3850a6c
isu217 <isu217@lehigh.edu>
2014-08-31 22:33:45 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+//Ian Udvarhelyi
+//isu217
+//CSE2
+//hw01
+//8/31/14
+//
+
+//define a class
+public class WelcomeClass {
+
+//define main method
+public static void main(String [] args) {
+    
+    //pring out welcome statement
+    System.out.println("    -----------");
+    System.out.println("    | WELCOME |");
+    System.out.println("    -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-I--S--U--2--1--7-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This past summer I went fishing in Georgia and caught sharks");
+    }
+}
