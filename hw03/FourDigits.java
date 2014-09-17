837d156655e0efcf06b97444ff0a3e1c07d72b6a
isu217 <isu217@lehigh.edu>
2014-09-17 00:43:26 +0000

    updated hw03

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -7,15 +7,18 @@ import java.util.Scanner;
 
 public class FourDigits {
     public static void main(String[] args) {
-        Scanner myScanner;
+        Scanner myScanner; //initialize scanner
         myScanner=new Scanner(System.in);
+        //get user input
         System.out.print("Enter a double and I display 4 places to the right of the decimal: ");
         double number= myScanner.nextDouble();
         int intNumber= (int)number;
+        //manipulate user input
         number = number * 10000;
         intNumber *= 10000;
         int printNumber = (int)number - intNumber;
         double printNum = number % intNumber;
+        //print result
         System.out.println("The digits are "+ (int)printNum);
     }
     
--- hw03/Root.java
+++ hw03/Root.java
@@ -8,17 +8,21 @@ import java.util.Scanner;
 
 public class Root {
     public static void main(String[] args) {
-        Scanner myScanner;
+        Scanner myScanner; //initialize scanner
         myScanner=new Scanner(System.in);
         System.out.print("Enter a double and I print the cube root-");
+        //create variable from user input
         double number= myScanner.nextDouble();
         double guess= number/3;
+        //start computations of the guess
         guess= ((2*guess*guess*guess)+number)/(3*guess*guess);
         guess= ((2*guess*guess*guess)+number)/(3*guess*guess);
         guess= ((2*guess*guess*guess)+number)/(3*guess*guess);
         guess= ((2*guess*guess*guess)+number)/(3*guess*guess);
         guess= ((2*guess*guess*guess)+number)/(3*guess*guess);
+        //create final guess of the cube root
         double cube= guess*guess*guess;
+        //print out guess of cube root
         System.out.println("The cube root is: "+guess+":");
         System.out.println(+guess+"*"+guess+"*"+guess+"="+cube);
     }
